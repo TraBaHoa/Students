56be296756400c6460cd44e2a19697b2aad534ba
TraBaHoa <travinh572005@gmail.com>
2025-10-22 14:30:04 +0700

    Add files via upload

---
+++ Lab05_BUS/FacultyService.cs
@@ -0,0 +1,21 @@
+﻿using Lab05_DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab05_BUS
+{
+    public class FacultyService   
+    {
+        public List<Faculty> All
+        {
+            get
+            {
+                StudentModel context = new StudentModel();
+                return context.Faculties.ToList();
+            }
+        }
+    }
+}
---
+++ Lab05_BUS/StudentService.cs
@@ -0,0 +1,54 @@
+﻿using Lab05_DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.Entity.Migrations;
+namespace Lab05_BUS
+{
+    public class Student
+    {
+        public string AverageScore;
+        public object Faculty;
+        public object Major;
+        public object StudentID;
+
+        public object FullName { get; set; }
+        public string MajorName { get; set; }
+
+        public List<Lab05_DAL.Entities.Student> GetAll()
+        {
+            StudentModel context = new StudentModel();
+            return context.Students.ToList();
+        }
+        public List<Lab05_DAL.Entities.Student> GetAllHasNoMajor()
+        {
+            StudentModel context = new StudentModel();
+            return context.Students.Where(p => p.MajorID == null).ToList();
+        }
+
+        public List<Lab05_DAL.Entities.Student> GetAllHasNoMajorID(int facultyID)
+        {
+            StudentModel context = new StudentModel();
+            return context.Students.Where(p => p.MajorID == null && p.FacultyID == facultyID).ToList();
+        }
+
+        public Lab05_DAL.Entities.Student FindByID(string studentID)
+        {
+            StudentModel context = new StudentModel();
+            return context.Students.FirstOrDefault(p => p.StudentID == studentID);
+        }
+
+        public void InsertUpdate(Lab05_DAL.Entities.Student s)
+        {
+            StudentModel context = new StudentModel();
+            context.Students.AddOrUpdate(s);
+            context.SaveChanges();
+        }
+
+
+        
+    }
+}
